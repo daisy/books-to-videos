8646fc5aa97a1a91b1bdf8af5a0e1b49663b8361
Marisa DeMeglio <marisa.demeglio@gmail.com>
2021-07-18 15:30:37 -0700

    Fix: support daisy 2.02 books where the NCC entries point to a SMIL file with no #ID specifier after the filename

--- src/parser/daisy202/index.ts
+++ src/parser/daisy202/index.ts
@@ -1,14 +1,14 @@
 import path from 'path';
 import winston from 'winston';
-import * as utils from '../../utils';
-import * as types from "../../types";
-import { parse as parseNcc } from './ncc';
-import { parse as parseSmil } from './smil';
-import { parse as parseXHTML } from './xhtml';
+import * as types from "../../types/index.js";
+import * as utils from '../../utils/index.js';
+import { parse as parseNcc } from './ncc.js';
+import { parse as parseSmil } from './smil.js';
+import { parse as parseXHTML } from './xhtml.js';
 
 async function parse(nccFilename: string, settings: types.Settings): Promise<types.Book> {
     winston.info(`Parsing DAISY 2.02 book`);
-    let book:types.Book = await parseNcc(nccFilename, settings);
+    let book: types.Book = await parseNcc(nccFilename, settings);
 
     // resolve all the content URLs
     book.chapters.map(chapter => chapter.url = path.resolve(path.dirname(nccFilename), chapter.url));
@@ -17,18 +17,34 @@ async function parse(nccFilename: string, settings: types.Settings): Promise<typ
     let uniqueSmilUrls = new Set(book.chapters.map(chapter => utils.splitSrcSelector(chapter.url).src));
 
     // process all the SMIL files
-    let allSmilData = [];
+    let accSmilData = [];
     for (let smilUrl of uniqueSmilUrls) {
         let smilData = await parseSmil(smilUrl, settings);
-        allSmilData.push(smilData);
+        accSmilData.push(smilData);
     }
-    allSmilData = allSmilData.flat(); // in-order flat list of all media segments
+    let allSmilData: Array<types.MediaSegment> = accSmilData.flat(); // in-order flat list of all media segments
 
     let startIdx = -1;
     // assign media segment ranges to each chapter
     let startIdxs = book.chapters.map(chapter => {
+        if (chapter.url.indexOf("#") == -1) {
+            // if the chapter URL does not have a selector (e.g. file.smil#id), take the ID of the first occurence of that smil file
+            for (let mediaSegment of allSmilData) {
+                let found = false;
+                for (let id of mediaSegment.ids) {
+                    if (id.indexOf(chapter.url) != -1) {
+                        chapter.url = id;
+                        found = true;
+                        break;
+                    }
+                }
+                if (found) {
+                    break;
+                }
+            }
+        }
         if (allSmilData.length > startIdx + 1) {
-            startIdx = allSmilData.slice(startIdx+1).findIndex
+            startIdx = allSmilData.slice(startIdx + 1).findIndex
                 (mediaSegment => mediaSegment.ids.includes(chapter.url)) + startIdx + 1;
             return startIdx;
         }
@@ -38,8 +54,8 @@ async function parse(nccFilename: string, settings: types.Settings): Promise<typ
 
     for (let i = 0; i < startIdxs.length; i++) {
         let firstIdx = startIdxs[i];
-        let lastIdx = startIdxs.length > i + 1  ? startIdxs[i+1] - 1 : allSmilData.length - 1;
-        book.chapters[i].contents = allSmilData.slice(firstIdx, lastIdx+1);
+        let lastIdx = startIdxs.length > i + 1 ? startIdxs[i + 1] - 1 : allSmilData.length - 1;
+        book.chapters[i].contents = allSmilData.slice(firstIdx, lastIdx + 1);
     }
 
     // assign unique IDs to each phrase
@@ -60,4 +76,4 @@ async function parse(nccFilename: string, settings: types.Settings): Promise<typ
     return book;
 }
 
-export { parse }
+export { parse };
